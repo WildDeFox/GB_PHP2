c4ea913970ac947a2ea3c8119578c995eba7dd36
WildMrDeFold <wildmrdefox@gmail.com>
2023-04-13 21:43:16 +0700

    Version V3.0

--- cli.php
+++ cli.php
@@ -1,19 +1,2 @@
 <?php
 
-use Main\Component\Commands\CreateUserCommand;
-use Main\Component\Repositories\SqliteUsersRepository;
-use MongoDB\Driver\Exception\CommandException;
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-//$userRepository = new SqliteUsersRepository(
-//    new PDO('sqlite:' . __DIR__ . '/db.sqlite')
-//);
-$userRepository = new \Main\Component\Repositories\InMemoryUsersRepository();
-$command = new CreateUserCommand($userRepository);
-
-try {
-    $command->handle($argv);
-} catch (CommandException $e) {
-    echo "{$e->getMessage()}\n";
-}
