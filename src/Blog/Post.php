cf43c656821ff6e1751ed6fc76bf2456959c555f
WildMrDeFold <wildmrdefox@gmail.com>
2023-04-22 11:33:32 +0700

    Getter and setter для User и Post

--- src/Blog/Post.php
+++ src/Blog/Post.php
@@ -18,6 +18,55 @@ class Post
         $this->text = $text;
     }
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return Person
+     */
+    public function getAuthor(): Person
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param Person $author
+     */
+    public function setAuthor(Person $author): void
+    {
+        $this->author = $author;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
+
+
     public function __toString()
     {
         return $this->author . $this->id . ' пишет: ' . $this->text ;
--- src/Blog/User.php
+++ src/Blog/User.php
@@ -17,6 +17,54 @@ class User
         $this->login = $login;
     }
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return Name
+     */
+    public function getUsername(): Name
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param Name $username
+     */
+    public function setUsername(Name $username): void
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    /**
+     * @param string $login
+     */
+    public function setLogin(string $login): void
+    {
+        $this->login = $login;
+    }
+
     public function __toString(): string
     {
         return "Юзер $this->id с именем $this->username и логином $this->login." . PHP_EOL;
