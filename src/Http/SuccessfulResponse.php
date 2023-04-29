34ec452c8176c25064c50b63126e09d01d1fdb3a
WildMrDeFold <wildmrdefox@gmail.com>
2023-04-29 20:57:34 +0700

    Создан класс SuccessfulResponse

--- src/Http/SuccessfulResponse.php
+++ src/Http/SuccessfulResponse.php
@@ -5,5 +5,21 @@ namespace Main\Component\Http;
 
 class SuccessfulResponse extends  Response
 {
+    protected const SUCCESS = true;
+    private array $data = [];
+
+    // Успешный ответ содержит массив с данными,
+    // по умолчанию - пустой
+    public function __construct(array $data)
+    {
+        $this->data = $data;
+    }
+
+    // Реализация абстрактного метода
+    // родительского класса
+    protected function payload(): array
+    {
+        return ['data' => $this->data];
+    }
 
 }
