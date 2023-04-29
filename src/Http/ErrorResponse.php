4fb0584ec6271514c03f70b393d8ef4c27ef9e31
WildMrDeFold <wildmrdefox@gmail.com>
2023-04-29 21:01:40 +0700

    Создан класс ErrorResponse

--- src/Http/ErrorResponse.php
+++ src/Http/ErrorResponse.php
@@ -4,5 +4,19 @@ namespace Main\Component\Http;
 
 class ErrorResponse extends Response
 {
+    protected const SUCCESS = false;
+    private string $reason;
 
+    // Неуспешный ответ содержит строку с причиной неуспеха,
+    // по умолчанию - 'Something goes wrong'
+    public function __construct(string $reason = 'Something goes wrong')
+    {
+        $this->reason = $reason;
+    }
+    // Реализация абстрактного метода
+    // родительского класса
+    protected function payload(): array
+    {
+        return ['reason' => $this->reason];
+    }
 }
