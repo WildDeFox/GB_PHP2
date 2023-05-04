8163ad1819bcf0808f7dd7e793b1d9a40bc79ccd
WildMrDeFold <wildmrdefox@gmail.com>
2023-05-04 22:24:17 +0700

    fix

--- src/Http/ErrorResponse.php
+++ src/Http/ErrorResponse.php
@@ -6,7 +6,6 @@ class ErrorResponse extends Response
 {
     protected const SUCCESS = false;
     private string $reason;
-
     // Неуспешный ответ содержит строку с причиной неуспеха,
     // по умолчанию - 'Something goes wrong'
     public function __construct(string $reason = 'Something goes wrong')
